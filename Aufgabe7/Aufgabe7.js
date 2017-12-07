203e8c351ae75541f2a3b99caa0506cad4482c18
Alexander <Alexander@Alexander-PC>
2017-12-07 14:11:45 +0100

    Verbesserung A7

--- Aufgabe7/Aufgabe7.js
+++ Aufgabe7/Aufgabe7.js
@@ -26,7 +26,9 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        let dataArray = _input.split(","); //split gibt ein Array zur�ck
+        let dataArray = _input.split(","); //split: Teilt einen String in ein Array aus Substrings,
+        //welcher durch Kommas getrennt ist
+        //und gib das neue Array zur�ck
         let studentData = {
             matrikel: parseInt(dataArray[0]),
             lastname: dataArray[1],
@@ -35,26 +37,31 @@ var StudiVZ;
             gender: parseInt(dataArray[4]) == 0,
             comment: dataArray[5]
         };
+        if (Number.isNaN(studentData.matrikel)) {
+            return "Die Matrikelnummer muss eine Zahl sein";
+        }
         students.push(studentData);
         let gender;
         if (parseInt(dataArray[4]) == 0) {
             gender = "weiblich";
         }
         else {
-            gender = "m�nnlich";
+            gender = "m�nnlich"; //Eingabe nicht null = String wird in "m�nnlich" umgewandelt
         }
-        return "Deine Daten lauten:\n" + "\nMatrikelnummer: " + studentData.matrikel + "\nName: " + studentData.firstname + studentData.lastname + "\nAlter: " + studentData.age + "\nGeschlecht: " + gender + "\nKommentar: " + studentData.comment;
+        return "Deine Daten wurden erfolgreich gespeichert";
     }
     function queryData(_matrikel) {
         for (let i = 0; i < students.length; i++) {
+            //eingespeicherten Matrikel �bereinstimmt
             if (students[i].matrikel == _matrikel) {
-                let gender = students[i].gender ? "weiblich" : "m�nnlich";
+                let gender = students[i].gender ? "weiblich" : "m�nnlich"; //?-Operator pr�ft ob 0(weiblich) oder 1(m�nnlich) eingegeben wurde
                 return "Gefundene Daten zur Matrikelnummer: " + students[i].matrikel + "\n\nName: " + students[i].firstname + students[i].lastname + "\nAlter: " + students[i].age + "\nGeschlecht: " + gender + "\nKommentar: " + students[i].comment;
             }
             else {
-                return "Die eingegebene Matrikelnummer wurde noch nicht registriert";
+                continue; //Setzt For-Schleife fort 
             }
         }
+        return "Die eingegebene Matrikelnummer wurde noch nicht registriert";
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=Aufgabe7.js.map
--- Aufgabe7/Aufgabe7.ts
+++ Aufgabe7/Aufgabe7.ts
@@ -19,10 +19,10 @@ namespace StudiVZ {
     var students: StudentData[] = [];
     var stop: boolean = false;
 
-    while (!stop) {
+    while (!stop) {  //solange stop = false, Codeblock ausführen
         var action: string = prompt("Datensatz anlegen (n), abfragen (a) oder Programm beenden (s)\nn,a oder s eingeben");
 
-        switch (action) {
+        switch (action) {  //switch: Ausführen eines Codeblocks, abhängig von verschiedenen Fällen(cases)
             case "n":
             case "N":
                 var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 -> w oder 1 -> m) und Kommentar");
@@ -39,40 +39,48 @@ namespace StudiVZ {
         }
     }
 
-    function saveData(_input: string): string {
-        let dataArray: string[] = _input.split(",");   //split gibt ein Array zurück
+    function saveData(_input: string): string {  //Die Funktion gibt einen String zurück
+        let dataArray: string[] = _input.split(",");  //split: Teilt einen String in ein Array aus Substrings,
+                                                      //welcher durch Kommas getrennt ist
+                                                      //und gib das neue Array zurück
         let studentData: StudentData = {
-            matrikel: parseInt(dataArray[0]),          //parseInt wandelt einen String in eine Number um
-            lastname: dataArray[1],                    //nimmt mit [1] das zweite Element aus dem Array
-            firstname: dataArray[2],
+            matrikel: parseInt(dataArray[0]),  //parseInt wandelt einen String in eine Number um
+            lastname: dataArray[1],  //nimmt mit [1] das zweite Element aus dem Array
+            firstname: dataArray[2], //mit [2] das dritte, ...
             age: parseInt(dataArray[3]),
             gender: parseInt(dataArray[4]) == 0,
             comment: dataArray[5]
         };
 
+        if (Number.isNaN(studentData.matrikel)) {  //NaN = Not a Number, gibt einen Boolschen Wert aus (true/false)
+            return "Die Matrikelnummer muss eine Zahl sein";
+        }
+
         students.push(studentData);
 
         let gender: string;
-        if (parseInt(dataArray[4]) == 0) {
+        if (parseInt(dataArray[4]) == 0) {  //Bei Eingabe von 0 bei gender, wird 0 in String "weiblich" umgewandelt
             gender = "weiblich";
         }
         else {
-            gender = "männlich";
+            gender = "männlich";  //Eingabe nicht null = String wird in "männlich" umgewandelt
         }
 
-        return "Deine Daten lauten:\n" + "\nMatrikelnummer: " + studentData.matrikel + "\nName: " + studentData.firstname + studentData.lastname + "\nAlter: " + studentData.age + "\nGeschlecht: " + gender + "\nKommentar: " + studentData.comment;
+        return "Deine Daten wurden erfolgreich gespeichert";
     }
 
-    function queryData(_matrikel: number): string {
-        for (let i: number = 0; i < students.length; i++) {
-            if (students[i].matrikel == _matrikel) {
-                let gender: string = students[i].gender ? "weiblich" : "männlich";
+    function queryData(_matrikel: number): string {  //Funktion gibt einen String zurück
+        for (let i: number = 0; i < students.length; i++) {  //Prüft, ob eingegebene Matrikel mit einer bereits 
+                                                             //eingespeicherten Matrikel übereinstimmt
+            if (students[i].matrikel == _matrikel) {  //Wenn übereinstimmende Matrikel gefunden wurde...
+                let gender: string = students[i].gender ? "weiblich" : "männlich";  //?-Operator prüft ob 0(weiblich) oder 1(männlich) eingegeben wurde
                 return "Gefundene Daten zur Matrikelnummer: " + students[i].matrikel + "\n\nName: " + students[i].firstname + students[i].lastname + "\nAlter: " + students[i].age + "\nGeschlecht: " + gender + "\nKommentar: " + students[i].comment;
             }
 
-            else {
-                return "Die eingegebene Matrikelnummer wurde noch nicht registriert";
+            else {  //Wird eine noch nicht gespeicherte Matrikel eingegeben, kommt eine Fehlermeldung
+                continue;  //Setzt For-Schleife fort 
             }
         }
+        return "Die eingegebene Matrikelnummer wurde noch nicht registriert";
     }
 }
